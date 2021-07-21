a0fe0697b916b0cb9a66193c514e9bee72355200
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-07-21 18:53:48 +0530

    Create Randomnumber.py

---
+++ Randomnumber.py
@@ -0,0 +1,10 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
+
+Output
+5
+Please note it wont be 5always as the name itself tells random number
