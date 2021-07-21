c4be296020d8fc8117f3f56cd130cd0e7e595199
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-07-21 19:00:16 +0530

    Create °Cto°F.py

---
+++ °Cto°F.py
@@ -0,0 +1,14 @@
+Incase you dont forumla the forumala is celsius * 1.8 = fahrenheit - 32
+The input
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+Output
+celsius = (fahrenheit - 32) / 1.8
+
