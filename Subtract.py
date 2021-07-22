2be578f8c06181a69230bb55d2a5003542393876
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-07-22 07:52:56 +0530

    Create Subtract.py

---
+++ Subtract.py
@@ -0,0 +1,16 @@
+# Python program explaining
+# numpy.subtract() function
+
+import numpy as geek
+in_num1 = 4
+in_num2 = 6
+
+print ("1st Input number : ", in_num1)
+print ("2nd Input number : ", in_num2)
+	
+out_num = geek.subtract(in_num1, in_num2)
+print ("Difference of two input number : ", out_num)
+Output
+1st Input number :  4
+2nd Input number :  6
+Difference of two input number :  -2
